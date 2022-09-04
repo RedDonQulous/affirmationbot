c6697c2203dd0cac4b657690c64c22d85823545c
RedDonQulous <redquito2012@gmail.com>
2022-09-05 02:54:58 +0800

    Installed node-fetch library and implemented fetch on affirmation website.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 // Importing this allows you to access the environment variables of the running node process
 require("dotenv").config();
 
@@ -14,13 +16,23 @@ client.on("messageCreate", message => {
     // Here's I'm using one of An Idiot's Guide's basic command handlers. Using the PREFIX environment variable above, I can do the same as the bot token below
     if (message.author.bot) return;
 
-    message.reply("Received message: " + message.content);
-    const command = message.content;
+    const options = {
+        headers: {
+            Authorization: "Bearer " + process.env.DISCORD_TOKEN
+        }
+    };
 
-    if (command === "ping") {
-        message.reply("Pong!");
-    }
-
+    fetch(process.env.AFFIRMATION_URL, options)
+        .then( res => res.json() )
+        .then( data => {
+            // message.reply("Received message: " + message.content);
+            const command = message.content;
+        
+            if (command.includes("affirm")) {
+                message.reply(data.affirmation);
+            }
+        }
+    );
 })
 
 client.login(process.env.DISCORD_TOKEN);
